d6560c1f113397044569804187d5838115cadcf7
adammkeane <adamkeane.edu@gmail.com>
2023-10-09 11:37:37 +0000

    Add minsubarraylen solution

--- problem-solving-patterns/sliding-window/maxSubArraySum.js
+++ problem-solving-patterns/sliding-window/maxSubArraySum.js
@@ -1,8 +1,10 @@
 // Sliding Window - maxSubarraySum
 
-// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+// Given an array of integers and a number, write a function called maxSubarraySum, 
+// which finds the maximum sum of a subarray with the length of the number passed to the function.
 
-// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+// Note that a subarray must consist of consecutive elements from the original array. 
+// In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
 //     maxSubarraySum([100,200,300,400], 2) // 700
 //     maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)  // 39 
@@ -18,17 +20,17 @@
 
 // my solution
 
-function maxSubarraySum(a, n){
+function maxSubarraySum(a, n) {
     if (n > a.length) return null
     let temp = 0;
     let max = 0;
-    
+
     for (let i = 0; i < n; i++) {
         temp += a[i];
     }
-    
+
     for (let i = n; i < a.length; i++) {
-        temp = temp - a[i-n] + a[i]
+        temp = temp - a[i - n] + a[i]
         if (temp > max) {
             max = temp
         }
@@ -40,17 +42,17 @@ function maxSubarraySum(a, n){
 
 // maxSubArray Solution
 
-    function maxSubarraySum(arr, num){
-        if (arr.length < num) return null;
-     
-        let total = 0;
-        for (let i=0; i<num; i++){
-           total += arr[i];
-        }
-        let currentTotal = total;
-        for (let i = num; i < arr.length; i++) {
-           currentTotal += arr[i] - arr[i-num];
-           total = Math.max(total, currentTotal);
-        }
-        return total;
+function maxSubarraySum(arr, num) {
+    if (arr.length < num) return null;
+
+    let total = 0;
+    for (let i = 0; i < num; i++) {
+        total += arr[i];
     }
+    let currentTotal = total;
+    for (let i = num; i < arr.length; i++) {
+        currentTotal += arr[i] - arr[i - num];
+        total = Math.max(total, currentTotal);
+    }
+    return total;
+}
