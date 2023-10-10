b69e2ebe86860f0ecfb58bc37ab282dee1a6b005
adammkeane <adamkeane.edu@gmail.com>
2023-10-10 11:28:12 +0000

    Add findlongestsubstring solution

---
+++ problem-solving-patterns/sliding-window/findLongestSubString.js
@@ -0,0 +1,63 @@
+// Sliding Window - findLongestSubstring
+
+// Write a function called findLongestSubstring, 
+// which accepts a string and returns the length of the longest substring with all distinct characters.
+
+//     findLongestSubstring('') // 0
+//     findLongestSubstring('rithmschool') // 7
+//     findLongestSubstring('thisisawesome') // 6
+//     findLongestSubstring('thecatinthehat') // 7
+//     findLongestSubstring('bbbbbb') // 1
+//     findLongestSubstring('longestsubstring') // 8
+//     findLongestSubstring('thisishowwedoit') // 6
+
+// Time Complexity - O(n)
+
+// my solution
+
+function findLongestSubstring(s) {
+    if (s.length === 0) return 0
+
+    let start = 0;
+    let end = 0;
+    let ns = [];
+    let length = 0;
+    let temp = 0;
+
+    while (end <= s.length) {
+        if (new Set(ns).size < ns.length) {
+            ns.splice(0, 1);
+            start++
+        } else {
+            console.log(ns)
+            temp = ns.length
+            ns.push(s[end])
+            end++
+        }
+        if (temp > length) {
+            length = temp
+        }
+    }
+    return length
+}
+// official solution
+
+// findLongestSubstring Solution
+
+function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+        // index - beginning of substring + 1 (to include current in count)
+        longest = Math.max(longest, i - start + 1);
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1;
+    }
+    return longest;
+}
