473b23dfeda988e524bb73d296346ee22a73ac8d
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-12 14:37:19 +0000

    Add push for sll

--- data-structures/sll.js
+++ data-structures/sll.js
@@ -1,2 +1,38 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 // my solution
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    } 
+    push(val) {
+        let pushedNode = new Node(val);
+        if (this.head === null) {
+            this.head = pushedNode;
+            this.tail = pushedNode;
+        } else {
+            this.tail.next = pushedNode;
+            this.tail = pushedNode;
+        }
+        this.length++;
+        return this
+    }
+    pop() {
+        let poppedNode;
+        return poppedNode;
+    }
+}
+
+let list = new SinglyLinkedList();
+list.push(12);
+// console.log(list.push(32));
+console.log(list.pop())
+
 // official solution
