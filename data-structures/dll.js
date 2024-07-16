a01ab4ad0cebdbd4653c4407f4eb448ad1bf4bbd
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-16 16:26:00 +0000

    Add push to dll

--- data-structures/dll.js
+++ data-structures/dll.js
@@ -13,5 +13,29 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        let pushedNode = new Node(val);
+        if (!this.head) {
+            this.head = pushedNode;
+            this.tail = pushedNode;
+            console.log('h1', pushedNode.prev)
+        } else {
+            this.tail.next = pushedNode;
+            console.log('h2', pushedNode)
+            pushedNode.prev = this.tail;
+            console.log('h3', pushedNode)
+            this.tail = pushedNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+let list = new DoublyLinkedList();
+list.push(12);
+list.push(13);
+// list.push(14);
+// console.log(list.push(32));
+console.log(list)
+
 // official solution
