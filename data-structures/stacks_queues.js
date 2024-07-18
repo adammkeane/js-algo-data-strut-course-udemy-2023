13a34c7bd799d0c49424ec536704b048323753a9
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-18 08:59:49 +0000

    Add push to stack

--- data-structures/stacks_queues.js
+++ data-structures/stacks_queues.js
@@ -12,7 +12,27 @@ class Stack {
         this.last = null;
         this.size = 0;
     }
+    push(val) {
+        let pushedNode = new Node(val);
+        if (this.size === 0) {
+            this.first = pushedNode;
+            this.last = pushedNode;
+
+        } else {
+            pushedNode.next = this.first;
+            this.first = pushedNode;
+        }
+        this.size++;
+        return this.size;
+    }
 }
+
+let stack = new Stack();
+stack.push(11);
+stack.push(12);
+stack.push(13);
+console.log(stack.push(14));
+console.log(stack);
 
 
 
