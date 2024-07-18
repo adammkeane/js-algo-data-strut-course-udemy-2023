cf658c13c45952c7ebbe8d7838d17736413e98da
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-18 08:45:33 +0000

    Add basic classes for stack

--- data-structures/stacks_queues.js
+++ data-structures/stacks_queues.js
@@ -1,6 +1,18 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 // my solution
-
-
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+}
 
 
 
