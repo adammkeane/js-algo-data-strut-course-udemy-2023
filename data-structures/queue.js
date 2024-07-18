72aeaa71102377a2924b946bb04b4c37954366e1
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-18 11:27:41 +0000

    Add official solution to queue

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -42,9 +42,34 @@ queue.enqueue(13);
 console.log(queue.dequeue());
 console.log(queue);
 
-
-
-
-
-
 // official solution
+class QueueOfficial {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue(){
+        if(!this.first) return null;
+
+        var temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
