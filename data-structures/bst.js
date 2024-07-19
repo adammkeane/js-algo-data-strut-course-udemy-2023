2f0dc190f978e4797b0d20cf54db4131687f1da2
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-19 13:54:50 +0000

    Add insert to bst

--- data-structures/bst.js
+++ data-structures/bst.js
@@ -1,6 +1,6 @@
 class Node {
-    constructor(value) {
-        this.value = value;
+    constructor(val) {
+        this.val = val;
         this.left = null;
         this.right = null;
     }
@@ -11,14 +11,41 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(val) {
+        let insertedNode = new Node(val);
+        if (this.root === null) {
+            this.root = insertedNode;
+            return this;
+        }
+        let currentNode = this.root;
+        while (true) {
+            if (insertedNode.val < currentNode.val) {
+                if (currentNode.left === null) {
+                    currentNode.left = insertedNode; 
+                    break;
+                }
+                currentNode = currentNode.left;
+            } else {
+                if (currentNode.right === null) {
+                    currentNode.right = insertedNode;
+                    break;
+                }  
+                currentNode = currentNode.right;
+            }
+        }
+        return this;
+    }
 }
 
 
 let bst = new BinarySearchTree();
-bst.root = new Node(10);
-bst.root.right = new Node(15);
-bst.root.left = new Node(7);
-bst.root.left.right = new Node(9);
-
+// bst.root = new Node(10);
+// bst.root.right = new Node(15);
+// bst.root.left = new Node(7);
+// bst.root.left.right = new Node(9);
+console.log(bst.insert(11));
+console.log(bst.insert(12));
+console.log(bst.insert(9));
+// console.log(bst);
 
 // official solution
