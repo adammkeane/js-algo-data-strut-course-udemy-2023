02654d1653b61476098e0819b412a6c3659fe225
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-23 09:39:44 +0000

    Add basic class for binary heap

---
+++ data-structures/binary_heap.js
@@ -0,0 +1,13 @@
+// my solution
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+}
+
+let binaryHeap = new BinaryHeap();
+// console.log(binaryHeap);
+
+
+
+// official solution
--- data-structures/tree_traversal.js
+++ data-structures/tree_traversal.js
@@ -195,4 +195,60 @@ class BinarySearchTreeOfficial {
         }
         return false;
     }
+    BFS(){
+        var node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while(queue.length){
+           node = queue.shift();
+           data.push(node.value);
+           if(node.left) queue.push(node.left);
+           if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSInOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
+
+
+// var tree = new BinarySearchTree();
+// tree.insert(10);
+// tree.insert(6);
+// tree.insert(15);
+// tree.insert(3);
+// tree.insert(8);
+// tree.insert(20);
+// tree.DFSPreOrder();
+// tree.DFSPostOrder();
+// tree.DFSInOrder();
