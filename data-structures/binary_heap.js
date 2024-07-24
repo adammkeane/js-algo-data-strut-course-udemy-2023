f21d3eeedae8a39b013ce92a5d9bbc645d3cd7b4
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-07-24 08:29:25 +0000

    Add insert to binary heap

--- data-structures/binary_heap.js
+++ data-structures/binary_heap.js
@@ -1,11 +1,34 @@
+// swap function
+function swap (arr, idx1, idx2) {
+    let temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+}
+
 // my solution
 class MaxBinaryHeap {
     constructor() {
         this.values = [];
     }
+    insert(val) {
+        this.values.push(val);
+
+        function bubbleUp(arr, idx) {
+            let parentIndex = Math.floor((idx-1)/2);
+            while (arr[idx] > arr[parentIndex]) {
+                swap(arr, idx, parentIndex);
+                if (parentIndex > 0) bubbleUp(arr, parentIndex);
+            }
+        }
+
+        bubbleUp(this.values, this.values.length-1);
+        return this.values;
+    }
 }
 
-let binaryHeap = new BinaryHeap();
+let binaryHeap = new MaxBinaryHeap();
+binaryHeap.values = [41,39,33,18,27,12];
+console.log(binaryHeap.insert(55));
 // console.log(binaryHeap);
 
 
