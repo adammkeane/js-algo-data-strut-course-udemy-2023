85b2dc958e392159884ca3d779b92e0dc6903de3
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-08-05 19:38:17 +0000

    Add get method to hash tables

--- data-structures/hash_tables.js
+++ data-structures/hash_tables.js
@@ -14,16 +14,22 @@ class HashTable {
             }
         return total;
     }
-
     set(key, value) {
-        let hashedIndex = this._hash(key);
-        console.log(hashedIndex)
-        let arrayElement = this.keyMap[hashedIndex];
+        let index = this._hash(key);
+        let arrayElement = this.keyMap[index];
         if (Array.isArray(arrayElement)) {
             arrayElement.push([key, value]);
         } else {
-            this.keyMap[hashedIndex] = [[key, value]];
+            this.keyMap[index] = [[key, value]];
         }
+    }
+    get(key) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) return undefined;
+        for (let i=0; i < this.keyMap[index].length; i++) {
+            if (this.keyMap[index][i][0] === key) return this.keyMap[index][i][1];
+        }
+        return undefined;
     }
 }
 
@@ -31,6 +37,6 @@ let hashTable = new HashTable();
 hashTable.set('first', 'test1');
 hashTable.set('first', 'test1');
 console.log(hashTable.keyMap);
-console.log(hashTable.keyMap[14]);
+console.log(hashTable.get('first'));
 
 // official solution
