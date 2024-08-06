30a2fefdbf056641819a99688edcfe84d360452e
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-08-06 19:31:11 +0000

    Add remove edge method to graph

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -6,9 +6,13 @@ class Graph {
     addVertex(vertex) {
         this.adjacencyList[vertex] = [];
     }
-    addEdge(vertex1, vertex2) {
-        this.adjacencyList[vertex1].push(vertex2);
-        this.adjacencyList[vertex2].push(vertex1);
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(val => val !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(val => val !== v1);
     }
 }
 
@@ -21,4 +25,5 @@ g.addVertex("Milan")
 g.addVertex("Aspen")
 g.addEdge("Tokyo", "Milan")
 g.addEdge("Tokyo", "Aspen")
+g.removeEdge("Tokyo", "Milan")
 console.log(g)
