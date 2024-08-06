781ffaabdc0fadb22099e365961b167ef964fd7f
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-08-06 18:51:02 +0000

    Add add vertex method to graph

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -3,7 +3,15 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+    addVertex(str) {
+        this.adjacencyList[str] = [];
+    }
 }
 
 
 // official solution
+
+let g = new Graph();
+g.addVertex("Tokyo")
+g.addVertex("Milan")
+console.log(g)
