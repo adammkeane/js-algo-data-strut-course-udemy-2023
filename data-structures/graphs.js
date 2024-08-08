a1ff02176697fffb05a64e414e0b0df552701a03
Adam Keane <109616691+adammkeane@users.noreply.github.com>
2024-08-08 17:51:18 +0000

    add dsf recursvie method to graph

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -20,19 +20,59 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    DFS_Recusive(startNode) {
+        let results = [];
+        let visited = {};
+        let adjacencyList = this.adjacencyList
+
+        function helperFunction(vertex) {
+            visited[vertex] = true;
+            results.push(vertex);
+            for (let edge of adjacencyList[vertex]) {
+                if (!visited[edge]) { 
+                    console.log(edge)
+                    helperFunction(edge);
+                }
+            } 
+        }
+        helperFunction(startNode);
+        return results;
+    }
 }
 
 
 let g = new Graph();
-g.addVertex("Tokyo")
-g.addVertex("Milan")
-g.addVertex("Aspen")
-g.addEdge("Tokyo", "Milan")
-g.addEdge("Tokyo", "Aspen")
-g.addEdge("Milan", "Aspen")
+
+// g.addVertex("Tokyo")
+// g.addVertex("Milan")
+// g.addVertex("Aspen")
+// g.addEdge("Tokyo", "Milan")
+// g.addEdge("Tokyo", "Aspen")
+// g.addEdge("Milan", "Aspen")
 // g.removeEdge("Tokyo", "Milan")
-g.removeVertex("Tokyo")
+// g.removeVertex("Tokyo")
+
+// console.log(g)
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
+
 console.log(g)
+console.log(g.DFS_Recusive("A"))
+
+
 
 
 // official solution
